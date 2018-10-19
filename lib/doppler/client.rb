f9af9f69ff55ecf70d08924c73b68e78d1f7a9f5
Brian Vallelunga <vallelungabrian@gmail.com>
2018-10-18 22:54:45 -0700

    smarter local environments capture

--- lib/doppler/client.rb
+++ lib/doppler/client.rb
@@ -1,4 +1,3 @@
-require "doppler/client/version"
 require 'net/https'
 require 'json'
 require 'set'
@@ -22,17 +21,19 @@ module Doppler
     @@environ_segment = '/environments/'
     @@max_retries = 0
 
-    def initialize(api_key, pipeline, environment, priority = Priority.remote, send_local_keys = true, ignore_keys = [])
+    def initialize(api_key, pipeline, environment, priority = Priority.remote, track_keys = [], ignore_keys = [])
         raise ArgumentError, 'api_key not string' unless api_key.is_a? String
         raise ArgumentError, 'pipeline not string' unless pipeline.is_a? String
         raise ArgumentError, 'api_key not string' unless environment.is_a? String
         raise ArgumentError, 'priority not numeric' unless priority.is_a? Numeric 
+        raise ArgumentError, 'track_keys not array' unless track_keys.is_a? Array 
+        raise ArgumentError, 'ignore_keys not array' unless ignore_keys.is_a? Array 
 
         @api_key = api_key
         @pipeline = pipeline
         @environment = environment
         @default_priority = priority
-        @send_local_keys = send_local_keys
+        @track_keys = track_keys.to_set
         @ignore_keys = ignore_keys.to_set
         @host = ENV[@@host_key] ? ENV[@@host_key] : @@default_host
         
@@ -45,7 +46,7 @@ module Doppler
         
         if @send_local_keys
             local_keys.each do |key, value|                
-                if not @ignore_keys.include?(key)
+                if @track_keys.include?(key)
                     keys_to_send[key] = value
                 end  
             end
@@ -60,22 +61,33 @@ module Doppler
 
     def get(key_name, priority = nil)
         priority = priority != nil ? priority : @default_priority
-
-        if @remote_keys.key?(key_name)
-            if priority == Priority.local
-                return ENV[key_name] ? ENV[key_name] : @remote_keys[key_name]
-            else
-                return @remote_keys[key_name]
-            end
+        value = nil
+        
+        if priority == Priority.local
+            value = ENV[key_name] ? ENV[key_name] : @remote_keys[key_name]
+        else
+            value = @remote_keys[key_name] ? @remote_keys[key_name] : ENV[key_name]
         end
         
         if not @ignore_keys.include?(key_name)
-            _request('/missing_key', {
-                'key_name' => key_name
-            })
+              if value != nil
+                if ENV[key_name] != @remote_keys[key_name]
+                    local_keys = {}
+                    local_keys[key_name] = ENV[key_name]
+                    
+                    _request('/track_key', {
+                        'local_keys' => local_keys
+                    })
+                end
+            else
+                _request('/missing_key', {
+                    'key_name' => key_name
+                })
+                
+            end
         end
 
-        return ENV[key_name]
+        return value
     end
 
     def _request(endpoint, body, retry_count=0)
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,14 +2,14 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "doppler/client"
 require "minitest/autorun"
 
-ENV['TESTER'] = "4567"
+ENV['TESTER'] = "123"
 
 doppler = Doppler::Client.new(
   api_key = "lK1obsC3Q1atBqAw5AoqXmLDXC77eigYqFZOlVz2",
   pipeline = "31",
   environment = "development_ruby",
   priority = Doppler::Priority.remote,
-  send_local_keys = true,
+  track_keys = [],
   ignore_keys = []
 )
 
